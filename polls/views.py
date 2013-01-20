4513eddd0d743eeb7771d2018e4d231e8ebaf90e
santpal <santpald@yahoo.com>
2013-01-20 17:38:29 +1100

    final commit

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
-from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 
 from polls.models import Poll
 
@@ -18,10 +21,30 @@ def index(request):
 def detail(request, poll_id):
     p = get_object_or_404(Poll,pk=poll_id)
     #return HttpResponse("You're looking at poll %s." % poll_id)
-    return render_to_response('polls/detail.html',{'poll':p})
+    return render_to_response('polls/detail.html',{'poll':p}, context_instance=RequestContext(request))
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
+
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+
+
+
--- urls.py
+++ urls.py
@@ -4,19 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$','polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-
-    # Examples:
-    # url(r'^$', 'trial.views.home', name='home'),
-    # url(r'^trial/', include('trial.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
